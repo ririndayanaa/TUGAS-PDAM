b7441bf349336b068ee0a19be5fb4b6086b7d735
ririndayanaa <73209219+ririndayanaa@users.noreply.github.com>
2020-12-29 22:16:14 +0700

    Add files via upload

---
+++ latihan.ririn.php
@@ -0,0 +1,32 @@
+<html>
+<table>
+<form method="post" action="latihan.php">
+	<tr>
+		<td>Masukkan Total Penggunaan air</td>
+		<td><input type="text" name="guna"/></td>
+	</tr>
+	<tr>
+		<td><input type="submit" name="proses" value="HITUNG" /></td>
+	</tr>
+</table>
+</html>
+
+<?php
+
+if(isset($_POST["proses"])){
+	$guna = $_POST["guna"];
+	if($guna<=30){
+		$biaya = $guna * 120;
+		echo "Total penggunaan Anda $guna dengan biaya  $biaya";
+	}else if($guna<=60){
+		$biaya = ($guna-30) * 150 + 3600;
+		echo "Total penggunaan Anda $guna dengan biaya  $biaya"; 
+	}else if($guna<=90){
+		$biaya = ($guna-60) * 190 + 3600 + 4500;
+		echo "Total penggunaan Anda $guna dengan biaya  $biaya"; 
+	}else{
+		$biaya = ($guna-90) * 240 + 3600 + 4500 + 5700; 
+		echo "Total penggunaan Anda $guna dengan biaya  $biaya";
+	}		
+}
+?>
